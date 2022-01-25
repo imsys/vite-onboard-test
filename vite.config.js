8cea4c78ebbc7d30e4c9d262c73a9116b3a17474
Arthur Helfstein Fragoso <arthur@life.net.br>
2022-01-25 16:33:31 -0300

    fix for a onboard dependece

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+
+
+export default {
+
+    resolve:{
+        alias:{
+            // ↓ see https://github.com/vitejs/vite/issues/6085
+            "@ensdomains/address-encoder": "@ensdomains/address-encoder/lib/index.umd.js",
+        },
+    },
+
+}
